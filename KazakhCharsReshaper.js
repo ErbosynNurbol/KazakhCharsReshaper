4815059f1f9ea4be3d059aa5ec072a2013b879b5
mauleta <mauleta@qq.com>
2016-02-16 23:59:40 +0800

    Update KazakhCharsReshaper.js

--- KazakhCharsReshaper.js
+++ KazakhCharsReshaper.js
@@ -122,6 +122,7 @@
             if (string.charAt(i) == "\u2E32") { result += "\u060C"; continue; }
             if (string.charAt(i) == "\u2E35") { result += "\u061B"; continue; }
             if (string.charAt(i) == "\u002D") { result += "\u0640"; continue; }
+            if (string.charAt(i) == "\uFBAC") { result += "\u06BE"; continue; }
             if (string.charAt(i) == "\uFEFB" || string.charAt(i) == "\uFEFC")
             {
                 result += "\u0644" + "\u0627";
@@ -154,6 +155,7 @@
             if (prev == "\u060C") { result += "\u2E32"; prev = current; prevEnd = true; continue; }
             if (prev == "\u061B") { result += "\u2E35"; prev = current; prevEnd = true; continue; }
             if (prev == "\u0640") { result += "\u002D"; prev = current; prevEnd = true; continue; }
+            if (prev == "\u06BE") { result += "\uFBAC"; prev = current; prevEnd = true; continue; }
             var jointCharIndex = getJointCharIndex(prevEnd, prev, current)
             if (jointCharIndex != -1) {
                 result += jointCharArray[jointCharIndex];
